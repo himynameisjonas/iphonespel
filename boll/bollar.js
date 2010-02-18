5a9fb72508f031c91301c1cdcecc24cbdab94045
Jonas Forsberg <himynameisjonas@gmail.com>
2010-02-18 13:33:08 +0100

    studsande boll

--- boll/bollar.js
+++ boll/bollar.js
@@ -1,14 +1,31 @@
 $(document).ready(function(){
-	boll = new Ball(10,10,1,45)
+	spel = new Playfield(15,320,480);
+	
 	function update () {
 		
-		boll.move();
-		boll.draw();
+		spel.update();
 		
 		t = setTimeout(function(){ update();},10)
 	}
 	update();
 })
+
+function Playfield (balls,x,y) {
+	var that = this; // Fullösning för this-referenser inom events
+	this.balls = [];
+	this.xsize = x;
+	this.ysize = y;
+	
+	for (var i = 0; i < balls; i++){
+		this.balls[i] = new Ball(150,150,5,Math.random()*360); //Math.random()*360
+	}
+	
+	this.update = function () {
+		for (var i = this.balls.length - 1; i >= 0; i--){
+			this.balls[i].update();
+		}
+	}
+}
 
 function Ball (x,y,s,d) {
 	var that = this; // Fullösning för this-referenser inom events
@@ -38,5 +55,31 @@ function Ball (x,y,s,d) {
 		that.dom.css("left",that.xpos).css("top",that.ypos)
 	}
 	
+	this.collision_wall = function (x,y) {
+		left = 0;
+		right = x;
+		top = 0;
+		bottom = y;
+		
+		if ( (that.xpos+that.radius*2) >= right ) {
+			that.direction = 180-that.direction;
+		} else if (that.xpos <= 0 ) {
+			that.direction = 180-that.direction;
+		}
+		
+		if (that.ypos <= 0 ) {
+			that.direction = 360-that.direction;
+		} else if (that.ypos+that.radius*2 >= bottom) {
+			that.direction = 360-that.direction;
+		};
+		
+	}
+	
+	this.update = function () {
+		$("#info").text(that.direction)
+		that.collision_wall(320,480);
+		that.move();
+		that.draw();
+	}
 	
 }
