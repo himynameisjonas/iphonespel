fe4c955752b5aabb2bbe215c4691f23184ef72ec
kgm <kgm@kgm.cc>
2010-02-22 11:11:11 +0100

    test ontouchstart

--- whacky/whacky.js
+++ whacky/whacky.js
@@ -1,10 +1,8 @@
 $(document).ready(function(){
-	spel = new Playfield(4,320,6,480);
+	whacky = new Playfield(4,320,6,480);
 	
 	
 	function update () {
-		
-		spel.update();
 		
 		t = setTimeout(function(){ update();},10)
 	}
@@ -13,30 +11,38 @@ $(document).ready(function(){
 })
 
 function Square(x,y) {
+	
+	// "constructor"
 	var that = this;
 				
 	this.dom = $("<div />").
 		css("width", x+"px").
 		css("height",y+"px").
-		//css("background-color","red").
 		attr("class","square");
+		
+	// handle clicks
+	//this.update = function( )
+	
+	
+	this.dom.ontouchstart = function(evt){
+ 		 console.log(evt.pageX + "/" + evt.pageY);
+	}
+	
+	
 }
 
 
 // rutor i x-led, x, yled, y
 function Playfield (xsq,x,ysq,y) {
 	var that = this; // Fullösning för this-referenser inom events
-	console.log("here");
-	this.squares = new Array(ysq);
+
 	this.xsize = x;
 	this.ysize = y;
-	console.log(that.squares);
+	this.squares = new Array(ysq);
 	for (var i = 0; i < ysq; i++){
 		this.squares[i] = new Array( xsq );
-		for (var j = 0; j<xsq; j++ ) {
-			console.log( "skapar ny fyrkant");				
-			that.squares[i][j] = new Square( x/xsq, y/ysq );
-			
+		for (var j = 0; j<xsq; j++ ) {	
+			that.squares[i][j] = new Square( x/xsq, y/ysq );	
 			$("#playfield").append( that.squares[i][j].dom );
 		}
 	}
